52bcb1ee5b5d76036baf298577a39a82338378cc
almogs87 <almogs@gmail.com>
2024-01-19 10:15:53 +0200

    added more thresholds and pass/fail

--- main.py
+++ main.py
@@ -50,10 +50,13 @@ contrast_vec_median = np.arange(0)
 #     filename_contrast = str(filename[:-4] + idx + filename[-4:])
 #     cv2.imwrite(os.path.join(images_path,filename_contrast),img_contrast)
 median_size = 3
-images_path = 'images/AA/x'
+images_path = 'images/res_contrast'
 file_list = sorted(os.listdir(images_path))
 
-change_contrast = [-5,-2,0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30]
+# change_contrast = [-5,-2,0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30]
+# change_contrast = list(range(-10,32,2))
+change_contrast=list(range(len(file_list)))
+
 for k in file_list:
     filename = os.path.join(images_path,k)
     img = cv2.imread(filename)
@@ -65,6 +68,19 @@ for k in file_list:
 
     print(filename + ' original std = ' + str(score_img) +', median(' +str(median_size) + ')=' +str(score_img_median) )
 
+diff_median = np.diff(contrast_vec_median)
+calibrated_value = (change_contrast[np.argmax(diff_median)] + change_contrast[np.argmin(diff_median) + 1]) / 2
+AA_old = 15
+max_correction = 5
+spec = 1
+CalibrationInSafety = ((calibrated_value > AA_old-max_correction) & (calibrated_value < AA_old+max_correction))
+
+if (CalibrationInSafety > 0):
+    Status='Calibration succeeded'
+    color = 'green'
+else:
+    Status='Calibration out of spec, calibrate manually and repeat'
+    color = 'red'
 
 
 f,axarr = plt.subplots(2,2)
@@ -76,12 +92,30 @@ axarr[0][0].set_title("SEM measurements post-processing - original")
 axarr[0][0].set_xlabel(' % of orignal contrast image ')
 axarr[0][0].set_ylabel(" Contrast Value")
 axarr[0][0].grid()
+axarr[0][1].plot(change_contrast[1:],np.diff(contrast_vec),"--c")
 
-axarr[1][0].plot(change_contrast,contrast_vec_median,"-or")
+
+
+axarr[1][0].plot(change_contrast,contrast_vec_median,color ='b', marker ='o', markerfacecolor = 'c')
+axarr[1][0].text(5,25,Status,color=color, fontsize=10)
+
 axarr[1][0].set_xlabel(' % of orignal contrast image ')
 axarr[1][0].set_ylabel(" std Value - median(20)")
 axarr[1][0].set_title("SEM measurements post-processing - median(20)")
 axarr[1][0].grid()
+axarr[1][0].axvline(calibrated_value, color ='g')
+axarr[1][0].axvline(AA_old, color = 'r')
+axarr[1][0].axvline(AA_old-max_correction, color = 'k')
+axarr[1][0].axvline(change_contrast[np.argmin(diff_median)+1], color = 'm')
+axarr[1][0].axvline(AA_old-spec, color='y')
+
+
+axarr[1][0].axvline(change_contrast[np.argmax(diff_median)], color = 'm')
+axarr[1][0].axvline(AA_old+max_correction, color = 'k')
+axarr[1][0].axvline(AA_old+spec, color='y')
+
+
+axarr[1][0].legend(['median(20) STD','New Calibration','Previous Calibration','Safety','boundaries','spec'])
 
 
 plt.show()
